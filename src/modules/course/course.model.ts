e677804fc233a7507f42da22acd4f84f9f92a203
souravq <souravbera515@gmail.com>
2023-12-14 11:35:51 +0530

    Course Schema and Model Create done

---
+++ src/modules/course/course.model.ts
@@ -0,0 +1,35 @@
+// 2. Create a Schema corresponding to the document interface.
+
+import { Schema, model } from "mongoose";
+import { TCourse, TTag } from "./course.interface";
+
+const TagSchema = new Schema<TTag>({
+  name: { type: String },
+  isDeleted: { type: Boolean },
+});
+
+const CourseSchema = new Schema<TCourse>({
+  title: { type: String, required: true, unique: true },
+  instructor: { type: String },
+  categoryId: { type: Schema.Types.ObjectId },
+  price: { type: Number },
+  tags: { type: [TagSchema] },
+  startDate: { type: String },
+  endDate: { type: String },
+  language: { type: String },
+  provider: { type: String },
+  durationInWeeks: { type: Number },
+  details: {
+    level: {
+      type: String,
+      enum: ["Beginner", "Intermediate", "Advanced"],
+    },
+    description: {
+      type: String,
+    },
+  },
+});
+
+// 3. Create a Model.
+
+const Course = model<TCourse>("Course", CourseSchema);
