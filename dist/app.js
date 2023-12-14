b89a23f087db6b5b6da66593f085d831eca46e0b
souravq <souravbera515@gmail.com>
2023-12-14 14:47:40 +0530

    changes done

--- dist/app.js
+++ dist/app.js
@@ -5,11 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const course_route_1 = __importDefault(require("./modules/course/course.route"));
+const category_route_1 = require("./modules/category/category.route");
 const app = (0, express_1.default)();
 // Middleware
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+// Course
 app.use("/api/course", course_route_1.default);
+// Category
+app.use("/api/category", category_route_1.CategoryRouter.router);
 exports.default = app;
