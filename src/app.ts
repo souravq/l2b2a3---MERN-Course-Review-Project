afec1d6b0b632439ca0250501b10d3c363ad0165
souravq <souravbera515@gmail.com>
2023-12-15 16:50:43 +0530

    unused code remove from app file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,4 @@
 import express, { Application, Request, Response } from "express";
-//import router, { courseRouter } from "./modules/course/course.route";
-//import { CategoryRouter } from "./modules/category/category.route";
-//import { reviewRouter } from "./modules/review/review.route";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import notFound from "./middlewares/notFound";
 import router from "./routes";
@@ -15,22 +12,13 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-// Course
+// Common routes used
 app.use("/api", router);
-
-// Category
-
-// app.use("/api", CategoryRouter.router);
-
-// // Review
-
-// app.use("/api", reviewRouter.router);
 
 // Middleware - Error Handler
 app.use(globalErrorHandler);
 
 // Not found - Middleware
-
 app.use(notFound);
 
 export default app;
