4185128ec36acd0ef450932a657816db4f8ed5e3
souravq <souravbera515@gmail.com>
2023-12-18 15:47:41 +0530

    couse exist or not checking before creating review

--- src/modules/review/review.services.ts
+++ src/modules/review/review.services.ts
@@ -1,11 +1,17 @@
 // Create Review
 
+import { Course } from '../course/course.model'
 import { TReview } from './review.interface'
 import { Review } from './review.model'
 
 const createReviewIntoDB = async (reviewData: TReview) => {
+  const { courseId } = reviewData
   // eslint-disable-next-line no-useless-catch
   try {
+    const existingCourse = await Course.findById(courseId)
+    if (!existingCourse) {
+      throw new Error('Course not found')
+    }
     const result = await Review.create(reviewData)
     return result
   } catch (err) {
