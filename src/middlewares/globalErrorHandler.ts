1116ed6330e7f8ce88f144d076de70b1ab7d90ce
souravq <souravbera515@gmail.com>
2023-12-21 19:32:25 +0530

    Zod error response done

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,19 +1,47 @@
-import { NextFunction, Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ErrorRequestHandler } from 'express'
+import { ZodError } from 'zod'
+import { handleZodError } from '../error/handleZodError'
+import { handleValidationError } from '../error/handleValidationError'
+import { handleCastError } from '../error/handleCastError'
+import { handleDuplicateError } from '../error/handleDuplicateError'
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const success = false;
-  const message = err.message || "Something went wrong!!!";
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // Setting Default values
+  let statusCode = err.statusCode || 500
+  const success = false
+  let message = err.message || 'Something went wrong!!!'
+  let errorMessage = err.message || 'Something went wrong!!!'
 
-  return res.status(500).json({
+  if (err instanceof ZodError) {
+    const zodErrorDetails = handleZodError(err)
+    statusCode = zodErrorDetails?.statusCode
+    message = zodErrorDetails?.message
+    errorMessage = zodErrorDetails?.errorMessage
+  } else if (err?.name === 'ValidationError') {
+    const validationErrorDetails = handleValidationError(err)
+    statusCode = validationErrorDetails?.statusCode
+    message = validationErrorDetails?.message
+    errorMessage = validationErrorDetails?.errorMessage
+  } else if (err?.name === 'CastError') {
+    const validationErrorDetails = handleCastError(err)
+    statusCode = validationErrorDetails?.statusCode
+    message = validationErrorDetails?.message
+    errorMessage = validationErrorDetails?.errorMessage
+  } else if (err?.code === 11000) {
+    const validationErrorDetails = handleDuplicateError(err)
+    statusCode = validationErrorDetails?.statusCode
+    message = validationErrorDetails?.message
+    errorMessage = validationErrorDetails?.errorMessage
+  }
+
+  return res.status(statusCode).json({
     success,
     message,
+    errorMessage,
     errorDetails: err,
-  });
-};
+    stack: err.stack,
+  })
+}
 
-export default globalErrorHandler;
+export default globalErrorHandler
